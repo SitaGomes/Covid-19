d9d863c92146be741e59c242903a98249ecd30c1
SitaGomes <ansitagomes@gmail.com>
2021-08-25 17:21:51 +0100

    Add: Cors lib

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
 const MonthsRoute = require('./sixMonthsRoute')
 const WeeksRoute = require('./twoWeeksRoute')
 
 const app = express()
 
+app.use(cors())
 app.use(MonthsRoute)
 app.use(WeeksRoute)
 
